c15bbc237dcf7e40f12791703776703da6b66d42
mingang.he <dustgle@gmail.com>
2015-08-16 15:29:00 +0800

    remove unused code

--- src/tcfs_handler.erl
+++ src/tcfs_handler.erl
@@ -3,56 +3,12 @@
 
 -include_lib("kernel/include/file.hrl").
 
-readlink(Path) ->
-    file:read_link_all(Path).
-
-mkdir(Path, _Mode) ->
-    file:make_dir(Path).
-
-un_link(_Path) ->
-    {todo}.
-
-rmdir(Path) ->
-    file:del_dir(Path).
-
-symlink(Path, Link) ->
-    file:make_symlink(Path, Link).
-
-rename(Oldpath, Newpath) ->
-    file:rename(Oldpath, Newpath).
-
-%% Hard link
-make_link(Oldpath, Newpath) ->
-    file:make_link(Oldpath, Newpath).
-
-chmod(Path, Mode) ->
-    file:change_mode(Path, Mode).
-
-chown(Path, Uid, Gid) ->
-    file:change_owner(Path, Uid, Gid).
-
 truncate(Path, Newsize) ->
     {file_exists, true} = {file_exists, filelib:is_file(Path)},
     {ok, IO} = file:open(Path, [read, write]),
     {ok, _NewPosition} = file:position(IO, Newsize),
     ok = file:truncate(IO),
     file:close(IO).
-
-utime(Path, Mtime) ->
-    file:change_time(Path, Mtime).
-
-read(Path, Offset, Size) ->
-    {ok, F} = file:open(Path, [read, binary, raw]),
-    {ok, _Data} = file:pread(F, Offset, Size),
-    file:close(F).
-
-write(Path, Offset, _Size, Data) ->
-    {ok, F} = file:open(Path, [raw, write, binary]),
-    file:pwrite(F, Offset, Data),
-    file:close(F).
-
-readdir(Path) ->
-    file:list_dir_all(Path).
 
 %% see http://www.epochconverter.com/
 datetime_to_epoch_time(DateTime) ->
